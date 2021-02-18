cc0b7f280c9f9ea9d3990ff1f039e353cff6ecb9
Kurt Choi <choi.kurt3@gmail.com>
2021-02-18 01:17:41 -0500

    directory navigation via path breadcrumbs

--- src/components/drive/FolderBreadCrumbs.js
+++ src/components/drive/FolderBreadCrumbs.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import { Breadcrumb } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { ROOT } from '../../hooks/UseFolder';
 
 const FolderBreadCrumbs = ({ currentFolder }) => {
+  let path = currentFolder === ROOT ? [] : [ROOT]
+  if (currentFolder) {
+    path = [...path, ...currentFolder.path]
+  }
+
+  
+
   return (
     <Breadcrumb className="flex-grow-1 m-0" listProps={{ className: "bg-white pl-0 m-0" }}>
+
+      {path.map((folder, index) => (
+        <Breadcrumb.Item className="text-truncate d-inline-block" style={{ maxWidth: 150 }} 
+          key={folder.id} linkAs={Link} linkProps={{ to: folder.id ? `/folder/${folder.id}` : '/'}}
+        >
+          { folder.name }
+        </Breadcrumb.Item>
+      ))}
+
       { currentFolder && (
         <Breadcrumb.Item className="text-truncate d-inline-block" style={{ maxWidth: 200 }} active>
           { currentFolder.name }
